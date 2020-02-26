6e65a671c7d4e8c65007697d32651e4ce196fd6c
tk-yoshimura <52984388+tk-yoshimura@users.noreply.github.com>
2020-02-26 11:39:21 +0900

    + Preset.Image.Layer diff filter

---
+++ TensorShaderPreset/Image/Layer/Filter_Diff.cs
@@ -0,0 +1,31 @@
+﻿using TensorShader;
+
+namespace TensorShaderPreset.Image {
+    /// <summary>フィルタ</summary>
+    public static partial class Filter {
+
+        /// <summary>X方向微分フィルタ</summary>
+        public static Field DiffX(Field x, float scale) {
+            float[] kernel =
+                {
+                    0,      0, 0,
+                    +scale, 0, -scale,
+                    0,      0, 0,
+                };
+
+            return SpatialFilter(x, kwidth: 3, kheight: 3, kernel, name: "DiffX");
+        }
+
+        /// <summary>Y方向微分フィルタ</summary>
+        public static Field DiffY(Field x, float scale) {
+            float[] kernel =
+                {
+                   0, +scale, 0,
+                   0, 0,      0,
+                   0, -scale, 0,
+                };
+
+            return SpatialFilter(x, kwidth: 3, kheight: 3, kernel, name: "DiffY");
+        }
+    }
+}
