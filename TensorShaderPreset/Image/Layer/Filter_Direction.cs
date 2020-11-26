70ce87399c9e01c2499670af4ef5f30e34eb248e
tk-yoshimura <yoshimura8192@gmail.com>
2020-11-27 02:51:25 +0900

    Update Filter_Direction.cs

--- TensorShaderPreset/Image/Layer/Filter_Direction.cs
+++ TensorShaderPreset/Image/Layer/Filter_Direction.cs
@@ -8,9 +8,9 @@ namespace TensorShaderPreset.Image {
     /// <summary>フィルタ</summary>
     public static partial class Filter {
 
-        private static NdimArray<float> KernelDirection(double cosdir, double sindir, int ksize, double smooth) {
+        private static NdimArray<float> KernelDirection(double cosdir, double sindir, int ksize, double smoothness) {
             double g(double x, double y) {
-                return Math.Pow(2, -smooth * x * x - 4 * y * y);
+                return Math.Pow(2, -smoothness * x * x - 4 * y * y);
             }
 
             double h(double x, double y) {
@@ -34,67 +34,67 @@ namespace TensorShaderPreset.Image {
             return new NdimArray<float>(Shape.Kernel2D(1, 1, ksize, ksize), wval);
         }
 
-        /// <summary>8方向微分フィルタ</summary>
-        public static NdimArray<float> KernelDirection8(int ksize = 5, double smooth = 0.5) {
-            double[] cosdirs = { 
-                1.0, +0.9238795325112867, +0.7071067811865475, +0.3826834323650898, 
-                0.0, -0.3826834323650898, -0.7071067811865475, -0.9238795325112867 
-            };
+        /// <summary>2冪数方向微分フィルタ</summary>
+        /// <param name="dirs">方向分割数(2冪数かつ4以上256以下)</param>
+        /// <param name="ksize">フィルタサイズ</param>
+        /// <param name="smoothness">フィルタの方向滑らかさ</param>
+        public static NdimArray<float> KernelDirection(int dirs, int ksize = 5, double smoothness = 0.5) {
+            if (dirs < 4 || dirs > 256 || (dirs & (dirs - 1)) != 0) {
+                throw new ArgumentException(nameof(dirs));
+            }
 
-            double[] sindirs = { 
-                0.0, 0.3826834323650898, 0.7071067811865475, 0.9238795325112867, 
-                1.0, 0.9238795325112867, 0.7071067811865475, 0.3826834323650898 
-            };
+            (double[] c, double[] s) cossindir(int divdirs) {
+                double[] c = { 1, 0, -1 }, s = { 0, 1, 0 };
+                int n = 3;
 
-            NdimArray<float>[] arrays = new NdimArray<float>[8];
+                while (divdirs > 2) {
+                    int new_n = n * 2 - 1;
+                    double[] new_c = new double[new_n], new_s = new double[new_n];
+
+                    for (int i = 0; i < n; i++) {
+                        new_c[i * 2] = c[i];
+                        new_s[i * 2] = s[i];
+                    }
+
+                    for (int i = 1; i < n; i++) {
+                        double ca = c[i - 1], cb = c[i], sa = s[i - 1], sb = s[i];
+                        double c2 = ca * cb - sa * sb;
+
+                        new_c[i * 2 - 1] = ((i * 2 < n) ? 1 : -1) * Math.Sqrt((1 + c2) / 2);
+                        new_s[i * 2 - 1] = Math.Sqrt((1 - c2) / 2);
+                    }
+
+                    n = new_n;  c = new_c;  s = new_s;
+
+                    divdirs /= 2;
+                }
+
+                c = c.Take(c.Length - 1).ToArray();
+                s = s.Take(s.Length - 1).ToArray();
+
+                return (c, s);
+            }
+
+            (double[] cosdirs, double[] sindirs) = cossindir(dirs);
+
+            NdimArray<float>[] arrays = new NdimArray<float>[dirs];
 
             for (int i = 0; i < arrays.Length; i++) {
-                arrays[i] = KernelDirection(cosdirs[i], sindirs[i], ksize, smooth);
+                arrays[i] = KernelDirection(cosdirs[i], sindirs[i], ksize, smoothness);
             }
 
             return NdimArray<float>.Join(Axis.Kernel2D.OutChannels, arrays);
         }
 
-        /// <summary>8方向微分フィルタ</summary>
-        public static Field Direction8(Field x, int ksize = 5, double smooth = 0.5) {
+        /// <summary>2冪数方向微分フィルタ</summary>
+        /// <param name="x">入力フィールド(単チャネル)</param>
+        /// <param name="dirs">方向分割数(2冪数かつ4以上256以下)</param>
+        /// <param name="ksize">フィルタサイズ</param>
+        /// <param name="smoothness">フィルタの方向滑らかさ</param>
+        public static Field Direction(Field x, int dirs, int ksize = 5, double smoothness = 0.5) {
             VariableField w = new VariableField(
-                KernelDirection8(ksize, smooth),
-                name: "Direction8"
-            );
-
-            return Convolution2D(x, w);
-        }
-
-        /// <summary>16方向微分フィルタ</summary>
-        public static NdimArray<float> KernelDirection16(int ksize = 5, double smooth = 0.5) {
-            double[] cosdirs = {
-                1.0, +0.9807852804032304, +0.9238795325112867, +0.8314696123025452,
-                +0.7071067811865475, +0.5555702330196022, +0.3826834323650898, +0.1950903220161282,
-                0.0, -0.1950903220161282, -0.3826834323650898, -0.5555702330196022,
-                -0.7071067811865475, -0.8314696123025452, -0.9238795325112867, -0.9807852804032304 
-            };
-
-            double[] sindirs = { 
-                0.0, 0.1950903220161282, 0.3826834323650898, 0.5555702330196022,
-                0.7071067811865475, 0.8314696123025452, 0.9238795325112867, 0.9807852804032304,
-                1.0, 0.9807852804032304, 0.9238795325112867, 0.8314696123025452,
-                0.7071067811865475, 0.5555702330196022, 0.3826834323650898, 0.1950903220161282 
-            };
-
-            NdimArray<float>[] arrays = new NdimArray<float>[16];
-
-            for (int i = 0; i < arrays.Length; i++) {
-                arrays[i] = KernelDirection(cosdirs[i], sindirs[i], ksize, smooth);
-            }
-
-            return NdimArray<float>.Join(Axis.Kernel2D.OutChannels, arrays);
-        }
-
-        /// <summary>16方向微分フィルタ</summary>
-        public static Field Direction16(Field x, int ksize = 5, double smooth = 0.5) {
-            VariableField w = new VariableField(
-                KernelDirection16(ksize, smooth),
-                name: "Direction16"
+                KernelDirection(dirs, ksize, smoothness),
+                name: $"Direction{dirs}"
             );
 
             return Convolution2D(x, w);
