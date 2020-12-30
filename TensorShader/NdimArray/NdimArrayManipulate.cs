19a8f66758c7df331eafead274ea629d05f110cc
tk-yoshimura <yoshimura8192@gmail.com>
2020-12-31 01:35:01 +0900

    + main.ndimarray pad

---
+++ TensorShader/NdimArray/NdimArrayManipulate.cs
@@ -0,0 +1,176 @@
+﻿using System;
+
+namespace TensorShader {
+
+    /// <summary>多次元配列</summary>
+    public partial class NdimArray<T> {
+
+        /// <summary>ゼロパディング</summary>
+        public static NdimArray<T> ZeroPadding(NdimArray<T> arr, params (int, int)[] pads) {
+            if (arr.Ndim != pads.Length) {
+                throw new ArgumentException(ExceptionMessage.Argument(nameof(pads), arr.Ndim, pads.Length));
+            }
+
+            foreach ((int pa, int pb) in pads) {
+                if (pa < 0 || pb < 0) {
+                    throw new ArgumentException(nameof(pads));
+                }
+            }
+
+            for (int axis = 0; axis < arr.Ndim; axis++) {
+                (int pa, int pb) = pads[axis];
+
+                if (pa <= 0 && pb <= 0) {
+                    continue;
+                }
+
+                int[] s = arr.Shape;
+                s[axis] += pa + pb;
+                NdimArray<T> new_arr = new Shape(arr.Type, s);
+
+                int length = arr.Shape.Stride(axis + 1), new_length = new_arr.Shape.Stride(axis + 1);
+                int n = arr.Length / length, sft = pa * new_arr.Shape.Stride(axis);
+
+                for (int i = 0; i < n; i++) {
+                    Array.Copy(arr.Value, i * length, new_arr.Value, i * new_length + sft, length);
+                }
+
+                arr = new_arr;
+            }
+
+            return arr;
+        }
+
+        /// <summary>エッジパディング</summary>
+        public static NdimArray<T> EdgePadding(NdimArray<T> arr, params (int, int)[] pads) {
+            if (arr.Ndim != pads.Length) {
+                throw new ArgumentException(ExceptionMessage.Argument(nameof(pads), arr.Ndim, pads.Length));
+            }
+
+            foreach ((int pa, int pb) in pads) {
+                if (pa < 0 || pb < 0) {
+                    throw new ArgumentException(nameof(pads));
+                }
+            }
+
+            for (int axis = 0; axis < arr.Ndim; axis++) {
+                (int pa, int pb) = pads[axis];
+
+                if (pa <= 0 && pb <= 0) {
+                    continue;
+                }
+
+                int[] s = arr.Shape;
+                int l = s[axis];
+
+                s[axis] += pa + pb;
+                NdimArray<T> new_arr = new Shape(arr.Type, s);
+
+                int stride = arr.Shape.Stride(axis), new_stride = new_arr.Shape.Stride(axis);
+                int length = arr.Shape.Stride(axis + 1), new_length = new_arr.Shape.Stride(axis + 1);
+                int n = arr.Length / length, sft = pa * new_stride;
+
+                for (int i = 0; i < n; i++) {
+                    Array.Copy(arr.Value, i * length, new_arr.Value, i * new_length + sft, length);
+                }
+
+                for (int i = 0; i < n; i++) {
+                    for (int a = 0; a < pa; a++) {
+                        Array.Copy(arr.Value, i * length, new_arr.Value, i * new_length + a * new_stride, stride);
+                    }
+                    for (int b = 0; b < pb; b++) {
+                        Array.Copy(arr.Value, i * length + (l - 1) * stride, new_arr.Value, i * new_length + (l + pa + b) * new_stride, stride);
+                    }
+                }
+
+                arr = new_arr;
+            }
+
+            return arr;
+        }
+
+        /// <summary>ゼロパディング</summary>
+        public static NdimArray<T> ZeroPadding1D(NdimArray<T> arr, int pad_left, int pad_right) {
+            if (arr.Type != ShapeType.Map || arr.Ndim != 3) { 
+                throw new ArgumentException(ExceptionMessage.ShapeElements(arr.Shape, ("Ndim", 3), ("Type", ShapeType.Map)));
+            }
+
+            return ZeroPadding(arr, (0, 0), (pad_left, pad_right), (0, 0));
+        }
+
+        /// <summary>ゼロパディング</summary>
+        public static NdimArray<T> ZeroPadding1D(NdimArray<T> arr, int pad) {
+            return ZeroPadding1D(arr, pad, pad);
+        }
+
+        /// <summary>ゼロパディング</summary>
+        public static NdimArray<T> ZeroPadding2D(NdimArray<T> arr, int pad_left, int pad_right, int pad_top, int pad_bottom) {
+            if (arr.Type != ShapeType.Map || arr.Ndim != 4) { 
+                throw new ArgumentException(ExceptionMessage.ShapeElements(arr.Shape, ("Ndim", 4), ("Type", ShapeType.Map)));
+            }
+
+            return ZeroPadding(arr, (0, 0), (pad_left, pad_right), (pad_top, pad_bottom), (0, 0));
+        }
+
+        /// <summary>ゼロパディング</summary>
+        public static NdimArray<T> ZeroPadding2D(NdimArray<T> arr, int pad) {
+            return ZeroPadding2D(arr, pad, pad, pad, pad);
+        }
+
+        /// <summary>ゼロパディング</summary>
+        public static NdimArray<T> ZeroPadding3D(NdimArray<T> arr, int pad_left, int pad_right, int pad_top, int pad_bottom, int pad_front, int pad_rear) {
+            if (arr.Type != ShapeType.Map || arr.Ndim != 5) { 
+                throw new ArgumentException(ExceptionMessage.ShapeElements(arr.Shape, ("Ndim", 5), ("Type", ShapeType.Map)));
+            }
+
+            return ZeroPadding(arr, (0, 0), (pad_left, pad_right), (pad_top, pad_bottom), (pad_front, pad_rear), (0, 0));
+        }
+
+        /// <summary>ゼロパディング</summary>
+        public static NdimArray<T> ZeroPadding3D(NdimArray<T> arr, int pad) {
+            return ZeroPadding3D(arr, pad, pad, pad, pad, pad, pad);
+        }
+
+        /// <summary>エッジパディング</summary>
+        public static NdimArray<T> EdgePadding1D(NdimArray<T> arr, int pad_left, int pad_right) {
+            if (arr.Type != ShapeType.Map || arr.Ndim != 3) { 
+                throw new ArgumentException(ExceptionMessage.ShapeElements(arr.Shape, ("Ndim", 3), ("Type", ShapeType.Map)));
+            }
+
+            return EdgePadding(arr, (0, 0), (pad_left, pad_right), (0, 0));
+        }
+
+        /// <summary>エッジパディング</summary>
+        public static NdimArray<T> EdgePadding1D(NdimArray<T> arr, int pad) {
+            return EdgePadding1D(arr, pad, pad);
+        }
+
+        /// <summary>エッジパディング</summary>
+        public static NdimArray<T> EdgePadding2D(NdimArray<T> arr, int pad_left, int pad_right, int pad_top, int pad_bottom) {
+            if (arr.Type != ShapeType.Map || arr.Ndim != 4) { 
+                throw new ArgumentException(ExceptionMessage.ShapeElements(arr.Shape, ("Ndim", 4), ("Type", ShapeType.Map)));
+            }
+
+            return EdgePadding(arr, (0, 0), (pad_left, pad_right), (pad_top, pad_bottom), (0, 0));
+        }
+
+        /// <summary>エッジパディング</summary>
+        public static NdimArray<T> EdgePadding2D(NdimArray<T> arr, int pad) {
+            return EdgePadding2D(arr, pad, pad, pad, pad);
+        }
+
+        /// <summary>エッジパディング</summary>
+        public static NdimArray<T> EdgePadding3D(NdimArray<T> arr, int pad_left, int pad_right, int pad_top, int pad_bottom, int pad_front, int pad_rear) {
+            if (arr.Type != ShapeType.Map || arr.Ndim != 5) { 
+                throw new ArgumentException(ExceptionMessage.ShapeElements(arr.Shape, ("Ndim", 5), ("Type", ShapeType.Map)));
+            }
+
+            return EdgePadding(arr, (0, 0), (pad_left, pad_right), (pad_top, pad_bottom), (pad_front, pad_rear), (0, 0));
+        }
+
+        /// <summary>エッジパディング</summary>
+        public static NdimArray<T> EdgePadding3D(NdimArray<T> arr, int pad) {
+            return EdgePadding3D(arr, pad, pad, pad, pad, pad, pad);
+        }
+    }
+}
---
+++ TensorShaderTest/NdimArray/NdimArrayManipulateTest.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using TensorShader;
+
+namespace TensorShaderTest {
+    [TestClass]
+    public class NdimArrayManipulateTest {
+        [TestMethod]
+        public void ZeroPaddingTest() {
+            Random random = new Random();
+
+            Shape shape = new Shape(ShapeType.Map, new int[] { 3, 4, 5, 6, 7 });
+            (int pa, int pb)[][] pads_list = new (int pa, int pb)[][]{
+                new (int pa, int pb)[] { (1, 2), (3, 4), (2, 2), (4, 3), (2, 1) },
+                new (int pa, int pb)[] { (0, 0), (3, 4), (2, 2), (4, 3), (2, 1) },
+                new (int pa, int pb)[] { (1, 2), (3, 4), (2, 2), (4, 3), (0, 0) },
+                new (int pa, int pb)[] { (1, 2), (3, 4), (0, 0), (4, 3), (2, 1) },
+                new (int pa, int pb)[] { (0, 1), (3, 4), (2, 2), (4, 3), (2, 1) },
+                new (int pa, int pb)[] { (1, 2), (3, 4), (2, 2), (4, 3), (0, 1) },
+                new (int pa, int pb)[] { (1, 2), (3, 4), (0, 1), (4, 3), (2, 1) },
+                new (int pa, int pb)[] { (1, 0), (3, 4), (2, 2), (4, 3), (2, 1) },
+                new (int pa, int pb)[] { (1, 2), (3, 4), (2, 2), (4, 3), (1, 0) },
+                new (int pa, int pb)[] { (1, 2), (3, 4), (1, 0), (4, 3), (2, 1) },
+            };
+
+            foreach (var pads in pads_list) {
+                float[] v = (new float[shape.Length]).Select((v) => (float)random.NextDouble()).ToArray();
+
+                NdimArray<float> arr = new NdimArray<float>(shape, v);
+                NdimArray<float> new_arr = NdimArray<float>.ZeroPadding(arr, pads);
+
+                Shape new_shape = new_arr.Shape;
+
+                for (int i4 = 0; i4 < new_shape[4]; i4++) {
+                    for (int i3 = 0; i3 < new_shape[3]; i3++) {
+                        for (int i2 = 0; i2 < new_shape[2]; i2++) {
+                            for (int i1 = 0; i1 < new_shape[1]; i1++) {
+                                for (int i0 = 0; i0 < new_shape[0]; i0++) {
+
+                                    if (i4 < pads[4].pa || i4 >= pads[4].pa + shape[4]) {
+                                        Assert.AreEqual(0f, new_arr[i4, i3, i2, i1, i0]);
+                                        continue;
+                                    }
+                                    if (i3 < pads[3].pa || i3 >= pads[3].pa + shape[3]) {
+                                        Assert.AreEqual(0f, new_arr[i4, i3, i2, i1, i0]);
+                                        continue;
+                                    }
+                                    if (i2 < pads[2].pa || i2 >= pads[2].pa + shape[2]) {
+                                        Assert.AreEqual(0f, new_arr[i4, i3, i2, i1, i0]);
+                                        continue;
+                                    }
+                                    if (i1 < pads[1].pa || i1 >= pads[1].pa + shape[1]) {
+                                        Assert.AreEqual(0f, new_arr[i4, i3, i2, i1, i0]);
+                                        continue;
+                                    }
+                                    if (i0 < pads[0].pa || i0 >= pads[0].pa + shape[0]) {
+                                        Assert.AreEqual(0f, new_arr[i4, i3, i2, i1, i0]);
+                                        continue;
+                                    }
+
+                                    Assert.AreEqual(
+                                        arr[i4 - pads[4].pa, i3 - pads[3].pa, i2 - pads[2].pa, i1 - pads[1].pa, i0 - pads[0].pa], 
+                                        new_arr[i4, i3, i2, i1, i0]
+                                    );
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void EdgePaddingTest() {
+            Random random = new Random();
+
+            Shape shape = new Shape(ShapeType.Map, new int[] { 3, 4, 5, 6, 7 });
+            (int pa, int pb)[][] pads_list = new (int pa, int pb)[][]{
+                new (int pa, int pb)[] { (1, 2), (3, 4), (2, 2), (4, 3), (2, 1) },
+                new (int pa, int pb)[] { (0, 0), (3, 4), (2, 2), (4, 3), (2, 1) },
+                new (int pa, int pb)[] { (1, 2), (3, 4), (2, 2), (4, 3), (0, 0) },
+                new (int pa, int pb)[] { (1, 2), (3, 4), (0, 0), (4, 3), (2, 1) },
+                new (int pa, int pb)[] { (0, 1), (3, 4), (2, 2), (4, 3), (2, 1) },
+                new (int pa, int pb)[] { (1, 2), (3, 4), (2, 2), (4, 3), (0, 1) },
+                new (int pa, int pb)[] { (1, 2), (3, 4), (0, 1), (4, 3), (2, 1) },
+                new (int pa, int pb)[] { (1, 0), (3, 4), (2, 2), (4, 3), (2, 1) },
+                new (int pa, int pb)[] { (1, 2), (3, 4), (2, 2), (4, 3), (1, 0) },
+                new (int pa, int pb)[] { (1, 2), (3, 4), (1, 0), (4, 3), (2, 1) },
+            };
+
+            foreach (var pads in pads_list) {
+                float[] v = (new float[shape.Length]).Select((v) => (float)random.NextDouble()).ToArray();
+
+                NdimArray<float> arr = new NdimArray<float>(shape, v);
+                NdimArray<float> new_arr = NdimArray<float>.EdgePadding(arr, pads);
+
+                Shape new_shape = new_arr.Shape;
+
+                for (int i4 = 0; i4 < new_shape[4]; i4++) {
+                    for (int i3 = 0; i3 < new_shape[3]; i3++) {
+                        for (int i2 = 0; i2 < new_shape[2]; i2++) {
+                            for (int i1 = 0; i1 < new_shape[1]; i1++) {
+                                for (int i0 = 0; i0 < new_shape[0]; i0++) {
+
+                                    Assert.AreEqual(
+                                        arr[
+                                            Math.Min(Math.Max(0, i4 - pads[4].pa), shape[4] - 1), 
+                                            Math.Min(Math.Max(0, i3 - pads[3].pa), shape[3] - 1), 
+                                            Math.Min(Math.Max(0, i2 - pads[2].pa), shape[2] - 1), 
+                                            Math.Min(Math.Max(0, i1 - pads[1].pa), shape[1] - 1), 
+                                            Math.Min(Math.Max(0, i0 - pads[0].pa), shape[0] - 1)
+                                        ], 
+                                        new_arr[i4, i3, i2, i1, i0]
+                                    );
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
