50bc503bf6041348a482d0d16eaf5c1a90306670
tk-yoshimura <yoshimura8192@gmail.com>
2021-01-04 00:24:37 +0900

    + utiltest patchworkflow d3

--- TensorShaderUtilTest/DataSplitUtil/PatchworkFlowTest.cs
+++ TensorShaderUtilTest/DataSplitUtil/PatchworkFlowTest.cs
@@ -82,10 +82,10 @@ namespace TensorShaderUtilTest.DataSplitUtil {
         [TestMethod]
         public void Execute2Dd2Test() {
             Shape[] shapes = new Shape[]{
-                Shape.Map2D(3, 59, 128, 2),
                 Shape.Map2D(3, 29,  41, 2),
                 Shape.Map2D(3, 30,  42, 2),
                 Shape.Map2D(3, 32,  64, 2),
+                Shape.Map2D(3, 59, 128, 2),
                 Shape.Map2D(3, 64, 128, 2),
                 Shape.Map2D(3, 96, 128, 2),
                 Shape.Map2D(3, 96,  42, 2),
@@ -113,6 +113,50 @@ namespace TensorShaderUtilTest.DataSplitUtil {
                                 Assert.AreEqual(
                                     (inmap[c, 2 * x, 2 * y, th] + inmap[c, 2 * x + 1, 2 * y, th] + inmap[c, 2 * x, 2 * y + 1, th] + inmap[c, 2 * x + 1, 2 * y + 1, th]) / 4 + 1, 
                                     outmap[c, x, y, th], $"{shape}, {c}, {x}, {y}, {th}"
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Execute2Dd3Test() {
+            Shape[] shapes = new Shape[]{
+                Shape.Map2D(3, 29,  41, 2),
+                Shape.Map2D(3, 30,  42, 2),
+                Shape.Map2D(3, 32,  64, 2),
+                Shape.Map2D(3, 59, 128, 2),
+                Shape.Map2D(3, 64, 128, 2),
+                Shape.Map2D(3, 96, 128, 2),
+                Shape.Map2D(3, 96,  42, 2),
+            };
+
+            VariableField input = Shape.Map2D(3, 30, 60, 2);
+            StoreField output = AveragePooling2D(input + 1, 3);
+
+            (Flow flow, _) = Flow.Inference(output);
+
+            PatchworkFlow patchwork = new PatchworkFlow(flow, input, output, new int[] { 4, 3 });
+
+            patchwork.ProgressEvent += Patchwork_ProgressEvent;
+
+            Random random = new Random();
+
+            foreach (Shape shape in shapes) {
+                NdimArray<float> inmap = (shape, (new float[shape.Length]).Select((_) => (float)random.Next(1, 16)).ToArray());
+                NdimArray<float> outmap = patchwork.Execute(inmap);
+
+                for (int th = 0; th < outmap.Batch; th++) {
+                    for (int y = 0; y < inmap.Height / 3; y++) { 
+                        for (int x = 0; x < inmap.Width / 3; x++) {
+                            for (int c = 0; c < outmap.Channels; c++) {
+                                Assert.AreEqual(
+                                    (inmap[c, 3 * x, 3 * y, th] + inmap[c, 3 * x + 1, 3 * y, th] + inmap[c, 3 * x + 2, 3 * y, th] + 
+                                     inmap[c, 3 * x, 3 * y + 1, th] + inmap[c, 3 * x + 1, 3 * y + 1, th] + inmap[c, 3 * x + 2, 3 * y + 1, th] + 
+                                     inmap[c, 3 * x, 3 * y + 2, th] + inmap[c, 3 * x + 1, 3 * y + 2, th] + inmap[c, 3 * x + 2, 3 * y + 2, th]) / 9 + 1, 
+                                    outmap[c, x, y, th], 1e-4f, $"{shape}, {c}, {x}, {y}, {th}"
                                 );
                             }
                         }
