8fc43ca14c543c5e1131f92dea1443629ac1728e
tk-yoshimura <yoshimura8192@gmail.com>
2021-01-12 17:26:54 +0900

    + test link.sort

--- TensorShader/Links/ArrayManipulation/Sort.cs
+++ TensorShader/Links/ArrayManipulation/Sort.cs
@@ -1,3 +1,4 @@
+using System;
 using static TensorShader.VariableNode;
 
 namespace TensorShader {
@@ -5,12 +6,18 @@ namespace TensorShader {
         /// <summary>ソート</summary>
         public static Field Sort(Field x, int axis) {
             Field y = new Field();
-            VariableField index = Tensor.Index(x.Shape, axis);
+            Link link;
 
-            Link link = new Links.ArrayManipulation.Sort(x, index, y, axis);
+            if (x.EnableBackprop) {
+                VariableField index = Tensor.Index(x.Shape, axis);
+
+                link = new Links.ArrayManipulation.SortWithBackprop(x, index, y, axis);
+            }
+            else {
+                link = new Links.ArrayManipulation.SortWithoutBackprop(x, y, axis);
+            }
 
             link.Forward();
-
             return y;
         }
     }
@@ -18,7 +25,7 @@ namespace TensorShader {
 
 namespace TensorShader.Links.ArrayManipulation {
     /// <summary>ソート</summary>
-    public class Sort : Link {
+    public class SortWithBackprop : Link {
         /// <summary>入力項</summary>
         protected Field X => InFields[0];
 
@@ -34,8 +41,13 @@ namespace TensorShader.Links.ArrayManipulation {
         private VariableNode index_sorted;
 
         /// <summary>コンストラクタ</summary>
-        public Sort(Field x, Field index, Field y, int axis)
+        public SortWithBackprop(Field x, Field index, Field y, int axis)
             : base(new Field[] { x, index }, y) {
+
+            if (!x.EnableBackprop) {
+                throw new ArgumentException($"{nameof(x)}.EnableBackprop");
+            }
+
             this.Axis = axis;
         }
 
@@ -44,8 +56,54 @@ namespace TensorShader.Links.ArrayManipulation {
             (VariableNode key, VariableNode value) = SortWithKey(X.Value, Index.Value, Axis);
 
             this.index_sorted = value;
-
+                
             Y.AssignValue(key);
+        }
+
+        /// <summary>逆伝搬</summary>
+        public override void Backward() {
+            if (Y.Grad == null) {
+                return;
+            }
+
+            if (X.EnableBackprop) {
+                (_, VariableNode value) = SortWithKey(index_sorted, Y.Grad, Axis);
+
+                X.AddGrad(value);
+            }
+        }
+    }
+
+    /// <summary>ソート</summary>
+    public class SortWithoutBackprop : Link {
+        /// <summary>入力項</summary>
+        protected Field X => InFields[0];
+
+        /// <summary>インデクス</summary>
+        protected Field Index => InFields[1];
+
+        /// <summary>出力項</summary>
+        protected Field Y => OutField;
+
+        /// <summary>軸</summary>
+        protected readonly int Axis;
+
+        /// <summary>コンストラクタ</summary>
+        public SortWithoutBackprop(Field x, Field y, int axis)
+            : base(new Field[] { x }, y) {
+
+            if (x.EnableBackprop) {
+                throw new ArgumentException($"{nameof(x)}.EnableBackprop");
+            }
+
+            this.Axis = axis;
+        }
+
+        /// <summary>順伝搬</summary>
+        public override void Forward() {
+            VariableNode y = Sort(X.Value, Axis);
+    
+            Y.AssignValue(y);
         }
 
         /// <summary>逆伝搬</summary>
