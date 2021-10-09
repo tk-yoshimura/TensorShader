04228c4cb1d5553e76809404fd9497facc8276a3
tk-yoshimura <yoshimura8192@gmail.com>
2021-10-10 01:39:30 +0900

    Update CudaDll.cs

--- TensorShaderCudaBackend/Dll/CudaDll.cs
+++ TensorShaderCudaBackend/Dll/CudaDll.cs
@@ -4,35 +4,35 @@ using System.Runtime.InteropServices;
 
 namespace TensorShaderCudaBackend.Dll {
     static class CudaDll {
-        public static NativeDll Cuda { get; private set; }
-        public static NativeDll Nvcuda { get; private set; }
-        public static NativeDll Nvrtc { get; private set; }
+        public static NativeDll Cuda { get; private set; } = null;
+        public static NativeDll Nvcuda { get; private set; } = null;
+        public static NativeDll Nvrtc { get; private set; } = null;
 
         static CudaDll() {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
-                Nvcuda = new NativeDll("nvcuda.dll");
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
-                Nvcuda = new NativeDll("libcuda.so");
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
-                Nvcuda = new NativeDll("libcuda.dylib");
-            }
-            else { 
-                throw new NotSupportedException(RuntimeInformation.OSDescription);
-            }
-
-            foreach ((string cuda, string nvrtc) in LibraryNames) {
-                if (!NativeDll.Exists(cuda) || !NativeDll.Exists(nvrtc)) {
-                    continue;
+            foreach (string libname in NvcudaLibraryNames) {
+                if (NativeDll.Exists(libname)) {
+                    Nvcuda = new NativeDll(libname);
+                    break;
                 }
-
-                Cuda = new NativeDll(cuda);
-                Nvrtc = new NativeDll(nvrtc);
-                return;
             }
 
-            throw new DllNotFoundException("Not found cuda library. (major version=10,11)");
+            foreach (string libname in CudaLibraryNames) {
+                if (NativeDll.Exists(libname)) {
+                    Cuda = new NativeDll(libname);
+                    break;
+                }
+            }
+
+            foreach (string libname in NvrtcLibraryNames) {
+                if (NativeDll.Exists(libname)) {
+                    Nvrtc = new NativeDll(libname);
+                    break;
+                }
+            }
+
+            if (Nvcuda is null || Cuda is null || Nvrtc is null) {
+                throw new DllNotFoundException("Not found cuda library. (major version=10,11)");
+            }
         }
 
         static IEnumerable<(int major, int minor)> VersionList {
@@ -45,28 +45,74 @@ namespace TensorShaderCudaBackend.Dll {
             }
         }
 
-        static IEnumerable<(string cuda, string nvrtc)> LibraryNames {
+        static IEnumerable<string> NvcudaLibraryNames {
+            get {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                    yield return "nvcuda.dll";
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+                    yield return "libcuda.so";
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+                    yield return "libcuda.dylib";
+                }
+                else {
+                    throw new NotSupportedException(RuntimeInformation.OSDescription);
+                }
+            }
+        }
+
+        static IEnumerable<string> CudaLibraryNames {
             get {
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
                     foreach ((int major, int minor) in VersionList) {
-                        yield return ($"cudart64_{major}{minor}.dll", $"nvrtc64_{major}{minor}_0.dll");
+                        yield return $"cudart64_{major}{minor}.dll";
                     }
 
-                    yield return ("cudart64.dll", "nvrtc64.dll");
+                    yield return "cudart64.dll";
                 }
                 else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
                     foreach ((int major, int minor) in VersionList) {
-                        yield return ($"libcudart.so.{major}.{minor}", $"libnvrtc.so.{major}.{minor}");
+                        yield return $"libcudart.so.{major}.{minor}";
                     }
 
-                    yield return ("libcudart.so", "libnvrtc.so");
+                    yield return "libcudart.so";
                 }
                 else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
                     foreach ((int major, int minor) in VersionList) {
-                        yield return ($"libcudart.{major}.{minor}.dylib", $"libnvrtc.{major}.{minor}.dylib");
+                        yield return $"libcudart.{major}.{minor}.dylib";
                     }
 
-                    yield return ("libcudart.dylib", "libnvrtc.dylib");
+                    yield return "libcudart.dylib";
+                }
+                else {
+                    throw new NotSupportedException(RuntimeInformation.OSDescription);
+                }
+            }
+        }
+
+        static IEnumerable<string> NvrtcLibraryNames {
+            get {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                    foreach ((int major, int minor) in VersionList) {
+                        yield return $"nvrtc64_{major}{minor}_0.dll";
+                    }
+
+                    yield return "nvrtc64.dll";
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+                    foreach ((int major, int minor) in VersionList) {
+                        yield return $"libnvrtc.so.{major}.{minor}";
+                    }
+
+                    yield return "libnvrtc.so";
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+                    foreach ((int major, int minor) in VersionList) {
+                        yield return $"libnvrtc.{major}.{minor}.dylib";
+                    }
+
+                    yield return "libnvrtc.dylib";
                 }
                 else {
                     throw new NotSupportedException(RuntimeInformation.OSDescription);
